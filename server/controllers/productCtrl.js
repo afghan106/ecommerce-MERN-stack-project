d78e41f17ef246d443cbe772ed061cc78ce9ce76
afghansa106 <afghansa106@gmail.com>
2024-08-31 00:24:45 -0700

    pagnition done with limite,startindex,endindex and total product that we have in the stock

--- server/controllers/productCtrl.js
+++ server/controllers/productCtrl.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import Product from '../model/Product.js';
+import { query } from 'express';
 
 export const productCtrl = asyncHandler(async (req, res) => {
   const {
@@ -9,6 +10,7 @@ export const productCtrl = asyncHandler(async (req, res) => {
     category,
     sizes,
     colors,
+    user,
     price,
     totalQty,
     totalSold,
@@ -29,7 +31,7 @@ export const productCtrl = asyncHandler(async (req, res) => {
     category,
     sizes,
     colors,
-    user: req.userAuthId, // Ensure this is set correctly
+    user:req.userAuthId, 
     price, 
     totalQty,
     totalSold,
@@ -38,7 +40,7 @@ export const productCtrl = asyncHandler(async (req, res) => {
   // Respond with the created product
   res.status(201).json({
     status: 'successful',
-    msg: 'User Loged in  successfully',
+    msg: 'Product added to stack  successfully',
     product,
   });
 });
@@ -48,9 +50,77 @@ export const productCtrl = asyncHandler(async (req, res) => {
 //access public
 
 export const getProductCtrl=asyncHandler(async(req,res)=>{
-  const products=await Product.find();
+
+//search product with our given option or name 
+
+//query
+let productsQuery=Product.find();
+
+//search by name
+if (req.query.name) {
+  productsQuery=await productsQuery.find({
+    name:{$regex:req.query.name,$options:'i'}
+  })
+}
+// filter by category
+if (req.query.category) {
+  productsQuery=await productsQuery.find({
+    name:{$regex:req.query.category,$options:'i'}
+  })
+}
+
+//filter the product with price range
+if(req.query.price){
+  const priceRange=req.query.price.split('-');
+   const price=productsQuery.find({
+    price:{$gte:priceRange[0],$lte:priceRange[1]}
+   })
+  }
+
+
+
+
+        //pagenition
+//page
+const page=parseInt(req.query.page)?parseInt(req.query.page):1
+// limite
+const limit=parseInt(req.query.limit)?parseInt(req.query.limit):10;
+//start index
+const startIndex=(page-1)*limit;
+// end indexedDB
+const endIndex=page*limit;
+//total
+const total=await Product.countDocuments(); 
+productsQuery=productsQuery.skip(startIndex).limit(limit);
+
+
+//pagenation result
+
+
+const pagination={};
+if (endIndex<total) {
+pagination.next={
+  page:page+1,
+  limit,
+}
+};
+if (startIndex>0) {
+  pagination.prev={
+    page:page-1,
+    limit,
+  }
+  };
+
+
+
+const products=await productsQuery;
   res.json({
     status:'success',
+    total,
+    result:products.length,
+    pagination,
+    message:"products fetched successfully",
     products,
   });
 });
+
